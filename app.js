1885ca0113e1b1399ccd47cfb50d06dd674af816
Karolyn Seredick <kseredick95@gmail.com>
2020-05-08 19:18:14 -0400

    Added render and output functions. Need readme.

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const Employee = require("./lib/Employee")
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+const team = [];
+
+// Get information for employees
 function getEmployeeInfo() {
     inquirer.prompt([
         {
@@ -35,6 +37,7 @@ function getEmployeeInfo() {
             message: "What is their email?"
         }
     ]).then(async function (data) {
+        //Asks job specific information and creates object of each employee
         let name = data.name;
         let id = data.id;
         let email = data.email;
@@ -52,7 +55,7 @@ function getEmployeeInfo() {
 
                 employee = new Engineer(name, id, email, data.github);
 
-                return employee;
+                team.push(employee);
             })
 
         }
@@ -67,7 +70,7 @@ function getEmployeeInfo() {
 
                 employee = new Intern(name, id, email, data.school);
 
-                return employee;
+                team.push(employee);
             })
 
         }
@@ -82,51 +85,45 @@ function getEmployeeInfo() {
 
                 employee = new Manager(name, id, email, data.office);
 
-                return employee;
+                team.push(employee);
             })
 
         }
-        console.log(employee)
+
+        addEmployee(team);
     })
 }
 
-
-function addEmployee() {
+//Will allow user to add another employee if prompt is answered yes, otherwise it will send information to htmlRenderer
+function addEmployee(team) {
     inquirer.prompt([
         {
             type: "confirm",
             name: "add",
             message: "Would you like to add another employee?"
         }
-    ]).then(function (data) {
+    ]).then(async function (data) {
         if (data.add) {
             getEmployeeInfo();
         }
         else {
-            console.log("No new employee")
+            const html = await render(team);
+            fileWrite(html);
+        }
+    })
+}
+
+//Writes file ("team.html") to output folder
+function fileWrite(html) {
+
+    fs.writeFile(outputPath, html, function (err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("Commit logged!");
         }
     })
 }
 
 getEmployeeInfo();
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!``
